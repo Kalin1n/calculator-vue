4aab769729fd008dfe3544a43de4f3fc7a24fc8a
Kalin1n <kalinin_ivan_donetsk@ukr.net>
2021-02-18 23:15:32 +0300

    start switch color mode

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="page">
+  <div class="page" v-bind:class="theme">
     <div class="wrapper">
       <div class="display">
         <span v-if="$store.state.operatorSign">
@@ -36,13 +36,28 @@
       <div @click="$store.commit('changeSign')" class="btn">+/-</div>
       <div @click="$store.commit('equal')" class="btn operator">=</div>
     </div>
+    <div class="switch">
+      <button @click="changeColorTheme">Switch color theme</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      theme: "light",
+    };
+  },
   methods: {
+    changeColorTheme() {
+      if (this.theme === "light") {
+        this.theme = "dark";
+      } else {
+        this.theme = "light";
+      }
+    },
     numberHandler(number) {
       this.$store.commit("numberHandler", { number: number });
     },
@@ -58,11 +73,17 @@ export default {
 .page {
   font-family: "Montserrat";
   padding: 20px;
-  background-color: darkcyan;
+  position: relative;
 }
+
+.switch {
+  position: absolute;
+  top: 40px;
+  right: 5%;
+}
+
 .wrapper {
   padding: 10px;
-  background-color: #5a7bbe;
   margin: 0 auto;
   width: 600px;
   height: 100vh;
@@ -71,10 +92,15 @@ export default {
   grid-auto-rows: minmax(50px, 100px);
   grid-gap: 10px;
   font-size: 30px;
+  background-color: #5a7bbe;
 }
 
-.wrapper::after {
-  background-color: black;
+.light {
+  background-color: darkcyan;
+}
+
+.dark {
+  background-color: #0d1117;
 }
 
 .zero {
